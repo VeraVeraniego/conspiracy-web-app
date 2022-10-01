196486a745ebecabee34ef5850ba5a33f6272595
Brayan Vera <brayanvera@ravn.co>
2022-09-30 23:36:51 -0500

    chore: add eslint and prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import AppRouter from './AppRouter'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
- const App = () => {
+const App = () => {
   return (
     <MainWrapper className="App">
       <AppRouter />
     </MainWrapper>
-  );
+  )
 }
 
-export default App;
+export default App
 
 const MainWrapper = styled.div`
-	width: 100%;
-	min-height: 100hv;
+  width: 100%;
+  min-height: 100hv;
 `

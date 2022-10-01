85d0dee0406a3034f075facd015a0866120cbedc
Brayan Vera <brayanvera@ravn.co>
2022-10-01 10:35:53 -0500

    feat: add Leaderboard sidebar

--- src/components/Dashboard/Events/Events.tsx
+++ src/components/Dashboard/Events/Events.tsx
@@ -34,9 +34,9 @@ export const Events = () => {
         <EventCard>
           <EventCardUserInfo>
             <CardUserInfoPoints>+1pt</CardUserInfoPoints>
-            <CadrUserInfoPicture>
+            <CardUserInfoPicture>
               <img src={profileImage} />
-            </CadrUserInfoPicture>
+            </CardUserInfoPicture>
             <CardUserEventInfo>
               <h3>
                 Pollito a la brasa •{' '}
@@ -159,12 +159,13 @@ const CardUserInfoPoints = styled.h2`
   color: ${theme.palette.gray.zero};
 `
 
-const CadrUserInfoPicture = styled.div`
-  width: 40px;
-  height: 40px;
+export const CardUserInfoPicture = styled.div<{ size?: number }>`
+  width: ${(props) => (props.size ? props.size : 40)}px;
+  height: ${(props) => (props.size ? props.size : 40)}px;
   border-radius: 50%;
   border: 2px solid ${theme.palette.gray.zero};
   overflow: hidden;
+  z-index: 15;
 
   & img {
     width: 100%;

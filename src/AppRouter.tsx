68b9ebac639afa66d9fdc384a69dc48dbd36d9b2
gonzalo-nelnet <gonzalo@ravn.co>
2022-09-30 22:37:47 -0500

    feat: basic project scaffolding

---
+++ src/AppRouter.tsx
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { CreateEvent } from "./components/CreateEvent/CreateEvent";
+import { Dashboard } from "./components/Dashboard/Dashboard";
+import { EventDetails } from "./components/EventDetails/EventDetails";
+import { Leaderboard } from "./components/Leaderboard/Leaderboard";
+
+const AppRouter = () => {
+	return (
+		<BrowserRouter>
+    <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="createEvent" element={<CreateEvent />} />
+				<Route path="eventDetails" element={<EventDetails />} />
+				<Route path="leaderboard" element={<Leaderboard />} />
+    </Routes>
+  </BrowserRouter>
+	)
+}
+
+export default AppRouter
